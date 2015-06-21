2dfac79bd6440bd3af8d0da51325fcd055e3d0da
NickYoung <yangshun1029@gmail.com>
2015-06-21 14:06:49 +0800

    aria2gui

---
+++ MacGap/AppDelegate.m
@@ -0,0 +1,59 @@
+//
+//  AppDelegate.m
+//  MG
+//
+//  Created by Tim Debo on 5/19/14.
+//
+//
+
+#import "AppDelegate.h"
+#import "WindowController.h"
+
+@implementation AppDelegate
+
+- (void)applicationWillFinishLaunching:(NSNotification *)aNotification
+{
+    //RUN ARIA2
+    NSArray *arguments;
+    arguments = [NSArray arrayWithObjects: @"--enable-rpc", @"--rpc-listen-all=true", @"--rpc-allow-origin-all", @"-c", @"-D", @"-d /Users/Nick/Downloads/Aria2", nil];
+    NSString *resourcesPath = [[NSBundle mainBundle] resourcePath];
+    NSString *exePath = [NSString stringWithFormat:@"%@/aria2c",resourcesPath];
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:exePath];
+    [task setArguments: arguments];
+    [task launch];}
+
+
+
+-(BOOL)applicationShouldHandleReopen:(NSApplication*)application
+                   hasVisibleWindows:(BOOL)visibleWindows{
+    if(!visibleWindows){
+        [self.windowController.window makeKeyAndOrderFront: nil];
+    }
+    return YES;
+}
+
+- (void) applicationDidFinishLaunching:(NSNotification *)aNotification {
+    self.windowController = [[WindowController alloc] initWithURL: kStartPage];
+    [self.windowController setWindowParams];
+    [self.windowController showWindow:self];
+    [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
+}
+
+-(void)applicationWillTerminate:(NSNotification *)notification
+{
+    //KILLALL ARIA2C
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/usr/bin/killall"];
+    [task setArguments:[NSArray arrayWithObject:@"aria2c"]];
+    [task launch];
+    [task waitUntilExit];
+}
+
+
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
+     shouldPresentNotification:(NSUserNotification *)notification
+{
+    return YES;
+}
+@end
---
+++ README.md
@@ -0,0 +1,7 @@
+[![Gitter chat](https://badges.gitter.im/MacGapProject/MacGap2.png)](https://gitter.im/MacGapProject/MacGap2)
+
+# Welcome to MacGap 2.
+
+For usage notes and API documentation, visit http://docs.macgap.com.
+
+To submit an issue or ask for help, go to the GitHub issue queue at https://github.com/MacGapProject/MacGap2/issues
