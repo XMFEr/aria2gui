164385d93e5d4d563a0b38211aa58f8c5fb31c40
NickYoung <yangshun1029@gmail.com>
2015-06-27 01:33:36 +0800

    update
    update

--- MacGap/AppDelegate.m
+++ MacGap/AppDelegate.m
@@ -13,16 +13,24 @@
 
 - (void)applicationWillFinishLaunching:(NSNotification *)aNotification
 {
+    //create directory aira2
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *theDestination  = [[NSHomeDirectory() stringByAppendingPathComponent:@"downloads"] stringByAppendingPathComponent:@"aria2"];
+    [fileManager createDirectoryAtPath: theDestination withIntermediateDirectories:YES attributes:nil error:nil];
+    
+    //设置默认路径
+    NSString *savePath = [@"~/Downloads/aria2" stringByExpandingTildeInPath];
+    
+    
     //RUN ARIA2
-    NSArray *arguments = [NSArray arrayWithObjects: @"--enable-rpc", @"--rpc-listen-all=true", @"--rpc-allow-origin-all", @"-c", @"-D", @"-d /Users/Nick/Downloads/Aria2", nil];
+    NSArray *arguments = [NSArray arrayWithObjects: @"--enable-rpc", @"--rpc-listen-all=true", @"--rpc-allow-origin-all", @"-c", @"-D", @"-d",savePath, nil];
     NSString *resourcesPath = [[NSBundle mainBundle] resourcePath];
     NSString *exePath = [NSString stringWithFormat:@"%@/aria2c",resourcesPath];
     NSTask *task = [[NSTask alloc] init];
     [task setLaunchPath:exePath];
     [task setArguments:arguments];
-    [task launch];}
-
-
+    [task launch];
+}
 
 -(BOOL)applicationShouldHandleReopen:(NSApplication*)application
                    hasVisibleWindows:(BOOL)visibleWindows{
@@ -38,7 +46,6 @@
     [self.windowController showWindow:self];
     [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
 }
-
 
 - (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
      shouldPresentNotification:(NSUserNotification *)notification
